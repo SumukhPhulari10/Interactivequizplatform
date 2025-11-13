b9b6bffcb5202036c5106e4b2557e4d22783c896
SumukhPhulari10 <sumukhphulari4@gmail.com>
2025-11-13 22:03:07 +0530

    Apply

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -2,7 +2,7 @@
 
 import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createBrowserClient(url!, anon!);
